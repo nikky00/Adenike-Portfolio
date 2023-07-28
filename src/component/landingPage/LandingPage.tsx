ba2607278022f46068c65365c7eababcbb8c4253
nikky00 <maryadenike081@gmail.com>
2023-07-28 16:16:27 +0100

    myportfolio

---
+++ src/component/landingPage/LandingPage.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const Landingpage = () => {
+  return (
+    <div className=''>
+    <div className="yarn">
+        <p>Adenike Okunola</p>
+        <div className="">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Work</li>
+                <li>Contact</li>
+                <li>Download</li>
+            </ul>
+        </div>
+    </div>
+
+    <div className="">
+        <h1>I am Adenike Okunola</h1>
+        <p>Frontend Developer/</p>
+    </div>
+        
+    </div>
+  )
+}
